f09e75026e5b0cce79633fff7cf4012753789535
Nikhil Singh <nikhil.singh@mongodb.com>
2020-10-13 16:50:52 +0530

    MongoDB Atlas and Azure VNET Peering

--- versions.tf
+++ versions.tf
@@ -3,6 +3,12 @@ terraform {
     mongodbatlas = {
       source = "terraform-providers/mongodbatlas"
     }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
   }
   required_version = ">= 0.13"
 }
